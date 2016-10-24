8db9a7f913d1a3600109413d618b6eb4e84d5293
Peter Kirschner <peter@kirschners.de>
2016-10-25 00:19:10 +0200

    ex4-solution

--- bnd/examples.bndtools.command/src/examples/bndtools/command/StringModifierCommand.java
+++ bnd/examples.bndtools.command/src/examples/bndtools/command/StringModifierCommand.java
@@ -1,18 +1,19 @@
 package examples.bndtools.command;
 
+import java.util.*;
 import org.osgi.service.component.annotations.*;
 import examples.service.api.StringModifier;
 
-@Component(property = { "osgi.command.scope" + "=zExampleModifier",
-		"osgi.command.function" + "=modify" }, service = StringModifierCommand.class)
+@Component(
+		property = { 
+				"osgi.command.scope:String=zExampleModifier",
+				"osgi.command.function:String=modify" 
+		}, 
+		service = StringModifierCommand.class
+)
 public class StringModifierCommand {
 
-	@Reference
-	private StringModifier modifier;
-
-	public void modify(String message) {
-		System.out.println(modifier.modify(message));
-	}
+	private List<StringModifier> modifier = new ArrayList<>();
 
 	@Activate
 	void activate() {
@@ -22,5 +23,25 @@ public class StringModifierCommand {
 	@Deactivate
 	void deactivate() {
 		System.out.println(this.getClass() + " deactivated");
+	}
+	
+	@Reference(
+		cardinality=ReferenceCardinality.AT_LEAST_ONE
+	)
+	void bindStringModifier(StringModifier modifier) {
+		this.modifier.add(modifier);
+	}
+	
+	void unbindStringModifier(StringModifier modifier) {
+		this.modifier.remove(modifier);
+	}
+	
+	void updatedStringModifier(StringModifier modifier, Map<String, Object> properties) {
+		System.out.println(modifier.getClass() + " configuration updated");
+		properties.forEach((key, value) -> System.out.println(key + " = " + value));
+	}
+
+	public void modify(String input) {
+		modifier.forEach(m -> System.out.println(m.modify(input)));
 	}
 }
