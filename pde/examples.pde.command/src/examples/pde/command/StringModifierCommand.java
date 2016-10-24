f85d02ceac22d3a71f3e0df0d283bea9ecd44a99
Peter Kirschner <peter@kirschners.de>
2016-10-24 23:47:45 +0200

    ex3-solutions

--- pde/examples.pde.command/src/examples/pde/command/StringModifierCommand.java
+++ pde/examples.pde.command/src/examples/pde/command/StringModifierCommand.java
@@ -1,44 +1,48 @@
 package examples.pde.command;
 
-import java.util.Map;
-
+import java.util.*;
 import org.osgi.service.component.annotations.*;
 import examples.service.api.StringModifier;
 
 @Component(
-	property = { 
-		"osgi.command.scope"    + "=zExampleModifier",
-		"osgi.command.function" + "=modify" }, 
-	service = StringModifierCommand.class
+		property = { 
+				"osgi.command.scope:String=zExampleModifier",
+				"osgi.command.function:String=modify" 
+		}, 
+		service = StringModifierCommand.class
 )
 public class StringModifierCommand {
 
-	private StringModifier modifier;
-
-	@Reference
-	void bindStringModifier(StringModifier modifier) {
-	    this.modifier = modifier;
-	}
-
-	void updatedStringModifier(StringModifier modifier, Map<String, ?> properties) {
-	    properties.forEach((k,v) -> {System.out.println(k + " : " + v);} );
-	}
-
-	void unbindStringModifier(StringModifier modifier) {
-	    this.modifier = null;
-	}
-
-	public void modify(String message) {
-	    System.out.println(modifier.modify(message));
-	}
+	private List<StringModifier> modifier = new ArrayList<>();
 
 	@Activate
 	void activate() {
-	    System.out.println(this.getClass() + " activated");
+		System.out.println(this.getClass() + " activated");
 	}
 
 	@Deactivate
 	void deactivate() {
-	    System.out.println(this.getClass() + " deactivated");
+		System.out.println(this.getClass() + " deactivated");
+	}
+	
+	@Reference(
+		cardinality=ReferenceCardinality.AT_LEAST_ONE,
+		policy=ReferencePolicy.DYNAMIC
+	)
+	void bindStringModifier(StringModifier modifier) {
+		this.modifier.add(modifier);
+	}
+	
+	void unbindStringModifier(StringModifier modifier) {
+		this.modifier.remove(modifier);
+	}
+	
+	void updatedStringModifier(StringModifier modifier, Map<String, Object> properties) {
+		System.out.println(modifier.getClass() + " configuration updated");
+		properties.forEach((key, value) -> System.out.println(key + " = " + value));
+	}
+
+	public void modify(String input) {
+		modifier.forEach(m -> System.out.println(m.modify(input)));
 	}
 }
